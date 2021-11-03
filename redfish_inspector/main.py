6aba889d4ae57931afafad0dec07c38136d244f6
Michael Sherman <shermanm@uchicago.edu>
2021-11-03 18:11:42 -0500

    set network ifaces to enabled

--- redfish_inspector/main.py
+++ redfish_inspector/main.py
@@ -20,14 +20,10 @@ from sushy.resources.system.storage.drive import Drive
 from redfish_inspector import referenceapi
 from redfish_inspector.redfish import (
     NetworkAdapter,
-    NetworkAdapterCollection,
     NetworkPort,
-    NetworkPortCollection,
     network_adapters,
-    PcieDevice,
     pcie_devices,
 )
-
 
 # Initialize and turn on debug logging
 openstack.enable_logging(debug=False)
@@ -44,7 +40,6 @@ def run():
             "driver_info",
             "properties",
         ],
-        "limit": "1",
     }
 
     conn: connection.Connection
@@ -57,8 +52,8 @@ def run():
             future_to_result = {
                 executor.submit(get_node_info, node): node
                 for node in nodes
-                # if ("nc16" in node.name) or ("GPU" in node.name)
-                if ("P3" in node.name)
+                if ("nc16" in node.name) or ("GPU" in node.name)
+                # if ("P3" in node.name)
             }
             for future in concurrent.futures.as_completed(future_to_result):
                 try:
@@ -67,45 +62,7 @@ def run():
                     print(exc)
 
 
-def _get_dell_oem_processor(proc: Processor) -> Mapping:
-    cpu_dict: Mapping = proc.json
-
-    try:
-        delloem_dict = cpu_dict.get("Oem", {}).get("Dell", {}).get("DellProcessor", {})
-
-        return delloem_dict
-    except KeyError:
-        return {}
-
-
-def _OneE3(mb: int):
-    try:
-        base = int(mb)
-        return int(base * 1e3)
-    except Exception as ex:
-        # print(ex)
-        return None
-
-
-def _OneE6(mb: int):
-    try:
-        base = int(mb)
-        return int(base * 1e6)
-    except Exception as ex:
-        # print(ex)
-        return None
-
-
-def _OneE9(mb: int):
-    try:
-        base = int(mb)
-        return int(base * 1e9)
-    except Exception as ex:
-        # print(ex)
-        return None
-
-
-def get_node_info(node):
+def get_node_info(node: Node):
 
     # print(node.name, node.id, node.properties)
     bmc_addr = node.driver_info.get("ipmi_address")
@@ -114,21 +71,7 @@ def get_node_info(node):
 
     base_url = f"https://{bmc_addr}/redfish/v1"
 
-    node_dict = {
-        "type": "node",
-        "uid": node.id,
-        "node_name": node.name,
-        "node_type": None,
-        "placement": {},
-        "network_adapters": [],
-        "storage_devices": [],
-        "pcie_devices": [],
-        "supported_job_types": {
-            "besteffort": False,
-            "deploy": True,
-            "virtual": "ivt",
-        },
-    }
+    reference_node = referenceapi.ChameleonBaremetal(node=node)
 
     try:
         conn = sushy.Sushy(
@@ -137,8 +80,6 @@ def get_node_info(node):
             password=bmc_password,
             verify=False,
         )
-        system = conn.get_system()
-        chassis = conn.get_chassis(CHASSIS_PATH)
     except ConnectionError:
         print(f"failed to connect to {node.name} at {bmc_addr}")
         raise
@@ -147,170 +88,123 @@ def get_node_info(node):
         raise
 
     print(f"querying {node.name} at {bmc_addr}")
+    system = conn.get_system()
+    reference_node.set_arch(system)
+    reference_node.set_bios(system)
+    reference_node.set_memory(system)
+    reference_node.set_monitoring()
 
     processors: List[Processor] = system.processors.get_members()
     cpu = processors[0]
+    reference_node.set_processor(cpu)
 
-    chassis_json: Mapping = chassis.json
-    location = chassis_json.get("Location")
+    chassis = conn.get_chassis(CHASSIS_PATH)
+    reference_node.set_chassis(chassis)
+    # reference_node.set_storage(chassis)
 
-    system_json: Mapping = system.json
-    oem_json: Mapping = system_json.get("Oem")
-
-    try:
-        bios_release_date = (
-            oem_json.get("Dell", {}).get("DellSystem", {}).get("BIOSReleaseDate")
-        )
-    except KeyError:
-        bios_release_date = None
-
-    node_dict["chassis"] = {
-        "manufacturer": system.manufacturer,
-        "name": chassis.model,
-        "serial": system.sku,
-    }
-
-    node_dict["bios"] = {
-        "release_date": bios_release_date,
-        "vendor": system.manufacturer,
-        "version": system.bios_version,
-    }
-
-    num_cpu = len(processors)
-    num_threads = int(cpu.total_threads) * num_cpu
-
-    instruction_set = re.sub("-bit$", "", str(cpu.instruction_set))
-    instruction_set = re.sub("\s", "_", instruction_set)
-
-    node_dict["architecture"] = {
-        "platform_type": instruction_set,
-        "smp_size": num_cpu,
-        "smt_size": num_threads,
-    }
-
-    dell_cpu_dict = _get_dell_oem_processor(cpu)
-
-    current_clock_speed_hz = _OneE6(dell_cpu_dict.get("CurrentClockSpeedMhz"))
-
-    node_dict["processor"] = {
-        "cache_l1": _OneE3(dell_cpu_dict.get("Cache1SizeKB")),
-        # "cache_l1d": None,
-        # "cache_l1i": None,
-        "cache_l2": _OneE3(dell_cpu_dict.get("Cache2SizeKB")),
-        "cache_l3": _OneE3(dell_cpu_dict.get("Cache3SizeKB")),
-        "clock_speed": current_clock_speed_hz,
-        "instruction_set": re.sub("_", "-", instruction_set),
-        "model": cpu.model,
-        # "other_description": cpu.model,
-        "vendor": cpu.manufacturer,
-        "version": None,
-    }
-
-    pcie_devs = pcie_devices(system=system)
-    for dev in pcie_devs:
-
-        # print(json.dumps(dev.json, indent=2))
-        # print(dev.pcie_functions)
-
-        if (
-            (dev.firmware_version)
-            or (dev.part_number)
-            or (dev.serial_number)
-            or ("NVIDIA" in dev.manufacturer)
-        ):
-
-            dev_name = dev.name
-            for func in dev.functions():
-                if func.function_id == 0:
-                    dev_name = func.name
-
-            dev_dict = {
-                "id": dev.identity,
-                "name": dev_name,
-                "manufacturer": dev.manufacturer,
-                "firmware_version": dev.firmware_version,
-                "part_number": dev.part_number,
-                "serial_number": dev.serial_number,
-            }
-
-            node_dict["pcie_devices"].append(dev_dict)
-
-    placement_keys = location.get("InfoFormat").split(";")
-    placement_vals = location.get("Info").split(";")
-    placement_dict = {}
-    for key, value in zip(placement_keys, placement_vals):
-        placement_dict[key] = value
-
-    node_dict["placement"] = {
-        "node": placement_dict.get("RackSlot"),
-        "rack": placement_dict.get("RackName"),
-    }
-
-    mem = system.memory_summary
-
-    node_dict["main_memory"] = {
-        "humanized_ram_size": f"{mem.size_gib} GiB",
-        "ram_size": _OneE9(mem.size_gib),
-    }
-    node_dict["monitoring"] = {}
+    ironic_mac = []
+    port_query = {"node_id": node.id, "fields": ["address"]}
+    # reuse connection
+    os_connection = node._connection
+    for os_port in os_connection.baremetal.ports(**port_query):
+        ironic_mac.append(os_port.address)
 
     adapters = network_adapters(chassis)
-    adapter: NetworkAdapter
     for adapter in adapters.get_members():
-        # print(json.dumps(adapter.json, indent=2))
-
         port: NetworkPort
         for port in adapter.ports().get_members():
-            # print(json.dumps(port.json, indent=2))
+            enabled = False
+            for mac in port.mac_address:
+                if str.lower(mac) in ironic_mac:
+                    enabled = True
+                    break
+            # print(port.mac_address, ironic_mac)
+            reference_node.add_network_port(adapter, port, enabled)
 
-            link_caps = port.link_capabilities
+    # reference_node.set_network(chassis)
 
-            port_dict = {
-                "device": port.identity,
-                "interface": link_caps[0].get("LinkNetworkTechnology"),
-                "mac": str.lower(port.mac_address[0]),
-                "model": adapter.model,
-                "rate": _OneE6(link_caps[0].get("LinkSpeedMbps")),
-                "vendor": adapter.manufacturer,
-            }
-            node_dict["network_adapters"].append(port_dict)
+    # chassis_json: Mapping = chassis.json
+    # location = chassis_json.get("Location")
 
-    for controller in system.storage.get_members():
-        drives = controller.drives
+    # system_json: Mapping = system.json
+    # oem_json: Mapping = system_json.get("Oem")
 
-        drive: Drive
-        for drive in drives:
-            storage_dict = {}
-            drive_dict: Mapping = drive.json
+    # pcie_devs = pcie_devices(system=system)
+    # for dev in pcie_devs:
 
-            size_in_gb = int(drive.capacity_bytes / (1e9))
+    #     # print(json.dumps(dev.json, indent=2))
+    #     # print(dev.pcie_functions)
 
-            storage_dict = {
-                "device": drive.identity,
-                # "driver": "megaraid_sas",
-                "humanized_size": f"{size_in_gb} GB",
-                "interface": drive_dict.get("Protocol"),
-                "model": drive.model,
-                "rev": drive_dict.get("Revision"),
-                "size": drive.capacity_bytes,
-                "vendor": drive.manufacturer,
-                "media_type": drive.media_type,
-            }
-            node_dict["storage_devices"].append(storage_dict)
+    #     if (
+    #         (dev.firmware_version)
+    #         or (dev.part_number)
+    #         or (dev.serial_number)
+    #         or ("NVIDIA" in dev.manufacturer)
+    #     ):
+
+    #         dev_name = dev.name
+    #         for func in dev.functions():
+    #             if func.function_id == 0:
+    #                 dev_name = func.name
+
+    #         dev_dict = {
+    #             "id": dev.identity,
+    #             "name": dev_name,
+    #             "manufacturer": dev.manufacturer,
+    #             "firmware_version": dev.firmware_version,
+    #             "part_number": dev.part_number,
+    #             "serial_number": dev.serial_number,
+    #         }
+
+    #         node_dict["pcie_devices"].append(dev_dict)
+
+    # placement_keys = location.get("InfoFormat").split(";")
+    # placement_vals = location.get("Info").split(";")
+    # placement_dict = {}
+    # for key, value in zip(placement_keys, placement_vals):
+    #     placement_dict[key] = value
+
+    # node_dict["placement"] = {
+    #     "node": placement_dict.get("RackSlot"),
+    #     "rack": placement_dict.get("RackName"),
+    # }
+
+    # for controller in system.storage.get_members():
+    #     drives = controller.drives
+
+    #     drive: Drive
+    #     for drive in drives:
+    #         storage_dict = {}
+    #         drive_dict: Mapping = drive.json
+
+    #         size_in_gb = int(drive.capacity_bytes / (1e9))
+
+    #         storage_dict = {
+    #             "device": drive.identity,
+    #             # "driver": "megaraid_sas",
+    #             "humanized_size": f"{size_in_gb} GB",
+    #             "interface": drive_dict.get("Protocol"),
+    #             "model": drive.model,
+    #             "rev": drive_dict.get("Revision"),
+    #             "size": drive.capacity_bytes,
+    #             "vendor": drive.manufacturer,
+    #             "media_type": drive.media_type,
+    #         }
+    #         node_dict["storage_devices"].append(storage_dict)
 
     # print(json.dumps(system.json, indent=2, sort_keys=True))
     # print(json.dumps(node_dict, indent=2, sort_keys=True))
 
-    node_obj = referenceapi.ChameleonBaremetal(node_dict)
+    # node_obj = referenceapi.ChameleonBaremetal(node_dict)
 
-    node_dict["node_type"] = node_obj.check_node_type()
+    # node_dict["node_type"] = node_obj.check_node_type()
 
-    gpu_dict = node_obj.get_gpus()
-    if gpu_dict.get("gpu"):
-        node_dict["gpu"] = gpu_dict
+    # gpu_dict = node_obj.get_gpus()
+    # if gpu_dict.get("gpu"):
+    #     node_dict["gpu"] = gpu_dict
 
-    if node_obj.check_infiniband():
-        node_dict["infiniband"] = True
+    # if node_obj.check_infiniband():
+    #     node_dict["infiniband"] = True
 
     # node_dict.pop("pcie_devices")
 
@@ -321,5 +215,5 @@ def get_node_info(node):
 
     output_file = Path(referencerepo_path, reference_filename)
     with open(output_file, "w+") as f:
-        json.dump(node_dict, f, indent=2, sort_keys=True)
+        json.dump(reference_node.json(), f, indent=2, sort_keys=True)
         print(f"generated {output_file}")
--- redfish_inspector/referenceapi.py
+++ redfish_inspector/referenceapi.py
@@ -2,6 +2,12 @@
 
 import json
 from typing import Mapping, List
+from openstack.baremetal.v1.node import Node
+from sushy.resources.chassis.chassis import Chassis
+from sushy.resources.system.system import System
+from sushy.resources.system import processor
+from sushy.resources.system.processor import Processor
+from redfish_inspector.redfish import NetworkPort, NetworkAdapter
 
 
 class G5kNode:
@@ -13,7 +19,24 @@ class G5kNode:
         return self.__dict__
 
 
+# class NetworkInterface:
+#     pass
+
+
+# class chassis:
+#     pass
+
+
+# class processor:
+#     pass
+
+
+# class Gpu:
+#     pass
+
+
 class ChameleonBaremetal(G5kNode):
+
     type = "node"
     supported_job_types = {
         "besteffort": False,
@@ -27,28 +50,107 @@ class ChameleonBaremetal(G5kNode):
         "serial": None,
     }
 
-    processor = {
-        "model": None,
-        "other_description": None,
-        "vendor": None,
-        "version": None,
-    }
+    # gpu = {
+    #     "gpu": False,
+    # }
 
-    gpu = {
-        "gpu": False,
-    }
+    # GPU_TYPES = (
+    #     "TU102GL [Quadro RTX 6000/8000]",
+    #     "GV100GL [Tesla V100 SXM2 32GB]",
+    # )
 
-    GPU_TYPES = (
-        "TU102GL [Quadro RTX 6000/8000]",
-        "GV100GL [Tesla V100 SXM2 32GB]",
-    )
+    def __init__(self, node: Node):
+        self.uid = node.id
+        self.node_name = node.name
+        self.type = ChameleonBaremetal.type
+        self.supported_job_types = ChameleonBaremetal.supported_job_types
+        self.network_adapters = []
+        self.pcie_devices = []
 
-    pcie_devices = []
-    network_adapters = []
-
-    def __init__(self, node_info: Mapping):
-        for key, value in node_info.items():
+    def from_dict(self, node_dict: Mapping):
+        for key, value in node_dict.items():
             setattr(self, key, value)
+
+    def set_arch(self, system: System):
+        cpu_summary = system.processors.summary
+        sockets = len(system.processors.get_members())
+
+        self.architecture = {
+            "platform_type": cpu_summary.architecture,
+            "smp_size": sockets,
+            "smt_size": cpu_summary.count,
+        }
+
+    def set_bios(self, system: System):
+        delloem = system.json.get("Oem", {}).get("Dell", {}).get("DellSystem", {})
+        self.bios = {
+            "release_date": delloem.get("BIOSReleaseDate", None),
+            "vendor": system.manufacturer,
+            "version": system.bios_version,
+        }
+
+    def set_memory(self, system: System):
+        mem = system.memory_summary
+        self.main_memory = {
+            "humanized_ram_size": f"{mem.size_gib} GiB",
+            "ram_size": int(mem.size_gib * 1e9),
+        }
+
+    def set_monitoring(self):
+        self.monitoring = {"wattmeter": False}
+
+    def set_processor(self, proc: Processor):
+        self.processor = {
+            "instruction_set": proc.instruction_set,
+            "model": proc.model,
+            "other_description": proc.model,
+            "vendor": proc.manufacturer,
+            "version": {
+                "family": proc.processor_id.effective_family,
+                "model": proc.processor_id.effective_model,
+                "cpuid": proc.processor_id.identification_registers,
+                "microcode": proc.processor_id.microcode_info,
+                "step": proc.processor_id.step,
+            },
+        }
+
+        delloem: Mapping = (
+            proc.json.get("Oem", {}).get("Dell", {}).get("DellProcessor", {})
+        )
+        if delloem:
+            self.processor.update(
+                {
+                    "clock_speed": int(delloem.get("CurrentClockSpeedMhz", 0) * 1e6),
+                    "cache_l1": int(delloem.get("Cache1SizeKB", 0) * 1e3),
+                    "cache_l2": int(delloem.get("Cache2SizeKB", 0) * 1e3),
+                    "cache_l3": int(delloem.get("Cache3SizeKB", 0) * 1e3),
+                }
+            )
+
+        # print(json.dumps(delloem, indent=2))
+
+    def set_chassis(self, chassis: Chassis):
+        self.chassis = {
+            "manufacturer": chassis.manufacturer,
+            "name": chassis.model,
+            "serial": chassis.sku,
+        }
+
+    def add_network_port(
+        self, adapter: NetworkAdapter, port: NetworkPort, enabled: bool
+    ):
+
+        link_caps = port.link_capabilities
+        port_dict = {
+            "device": port.identity,
+            "interface": link_caps[0].get("LinkNetworkTechnology"),
+            "mac": str.lower(port.mac_address[0]),
+            "model": adapter.model,
+            "rate": int(link_caps[0].get("LinkSpeedMbps") * 1e6),
+            "vendor": adapter.manufacturer,
+            "enabled": enabled,
+        }
+        self.network_adapters.append(port_dict)
 
     def get_gpus(self):
 
